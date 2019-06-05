5e01284043c363bd8b51833e35c0e5936fa2b5ec
sounishnath003 <flock.sinasini@gmail.com>
2019-06-05 15:50:02 +0530

    git is love <3

--- src/components/GetIss.vue
+++ src/components/GetIss.vue
@@ -56,11 +56,15 @@
             </div>
           </div>
       </div>
+      <!-- 
+        working map url
+        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2965.0824050173574!2d-93.63905729999999!3d41.998507000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sWebFilings%2C+University+Boulevard%2C+Ames%2C+IA!5e0!3m2!1sen!2sus!4v1390839289319"
+      -->
       <div class="map mt-16 w-full" id="googleMap">
         <h3 class="bg-yellow-400 text-xl text-center text-center p-2">Realtime Map Location</h3>
         <div style="width: 100%; margin-top: 50px;">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2965.0824050173574!2d-93.63905729999999!3d41.998507000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sWebFilings%2C+University+Boulevard%2C+Ames%2C+IA!5e0!3m2!1sen!2sus!4v1390839289319"
+            :src="map_url"
             width="100%"
             height="200"
             frameborder="0"
@@ -76,11 +80,13 @@
         >
         <div class="mt-8 md:text-left text-center">
           <div class="text-lg font-bold">Sounish Nath</div>
-          <div class="text-purple-500 mt-3">Student Developer</div>
+            <a href="https://github.com/sounishnath003">
+            <div class="text-purple-500 mt-3">Student Developer</div>
+          </a>   
           <div class="text-purple-500 mt-3">Kolkata, IN</div>
           <div class="text-gray-500 m-2">flock.sinasini@gmail.com</div>
           <div class="text-gray-500 m-2">(+91)-8017204855</div>
-          <div>**This site is under contruction</div>
+          <div>**This site is under construction</div>
         </div>
       </div>
     </div>
@@ -89,18 +95,24 @@
 
 
 <script>
+// https://www.google.com/maps/place/51%C2%B030'31.5%22N+0%C2%B007'15.1%22W/@51.508742,-0.1230387,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d51.508742!4d-0.12085
 import Axios from "axios";
 export default {
   data() {
     return {
       info: null,
       name: "",
+      map_info: null,
       error: false,
       latitude: "51.508742",
-      longitude: "-0.120850"
+      longitude: "-0.120850",
+      map_url: "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2965.0824050173574!2d-93.63905729999999!3d41.998507000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sWebFilings%2C+University+Boulevard%2C+Ames%2C+IA!5e0!3m2!1sen!2sus!4v1390839289319"
     };
   },
   methods: {
+    map_generator(latitude, longitude) {
+      return "https://maps.google.com/maps?q=+\'${this}\'+,+longitude+&hl=es;z=14&amp;output=embed"
+    },
     myMap() {
       var mapPop = {
         center: new google.maps.LatLng(this.latitude, this.longitude),
@@ -109,9 +121,9 @@ export default {
     }
   },
   created() {
-    Axios.get(`https://api.wheretheiss.at/v1/satellites/25544.json`)
+    Axios.get(`https://api.wheretheiss.at/v1/satellites/25544.json`, `https://ipapi.co/8.8.8.8/json/`)
       .then(response => {
-        this.info = response.data;
+        this.info = response.data
       })
       .catch(error => {
         this.error.push(error);
